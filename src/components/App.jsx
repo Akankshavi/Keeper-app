0eebf4291fdaef44e50f8524eab2e3ca508da003
Akanksha Hublikar <f20180317@hyderabad.bits-pilani.ac.in>
2021-05-09 18:19:18 +0530

    implement add and delete buttons

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,41 @@
 import React from "react";
 import Header from "./Header";
-import Note from "./Note";
 import Footer from "./Footer";
-import notes from "../notes";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
 
-function App(){
-    return (<div>
-    <Header />
-    {notes.map(note => (
+function App() {
+  const [notes, setNotes] = React.useState([]);
+  function addItem(note) {
+    console.log(notes);
+    setNotes((prevValues) => {
+      return [...prevValues, note];
+    });
+  }
+  function deleteItem(id) {
+    setNotes((prevValues) => {
+      return notes.filter((note, index) => {
+        return index !== id;
+      });
+    });
+  }
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addItem} />
+      {notes.map((note, index) => (
         <Note
-          key={note.id}
+          key={index}
+          id={index}
           title={note.title}
           content={note.content}
+          deleteItem={deleteItem}
         />
-    ))}
-    <Footer />
-    </div>);
+      ))}
+      {/* <Note key={1} title="Note title" content="Note content" /> */}
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
